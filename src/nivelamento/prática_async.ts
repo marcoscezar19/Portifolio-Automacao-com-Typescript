a961411b7de029b56270092d72236a20502c4809
Marcos Cézar <marcoscezar19@gmail.com>
2026-09-10 13:37:19 +0000

    atualizei nome do arquivo exercicioaula1.ts para exercicio_aula_1.ts

--- src/nivelamento/prática_async.ts
+++ src/nivelamento/prática_async.ts
@@ -1,2 +1,24 @@
-//IMportando função utilitária de aguardar tempo(delay)
+//IMPORTANDO FUNÇÃO UTILITÁRIA DE AGUARDAR TEMPO (DELAY)
 import { aguardar } from "../utils/helpers";
+//SIMULANDO UMA API DE LOGIN
+function simularLogin (usuario:string, senha:string):Proimise<string>{
+    return new Promise((resolve,reject) => {
+        if(usuário === 'admin' && senha === '123456'){
+            resolve('token-secreto-aprovado-123');
+        }else{
+            reject('ERRO 401 - USUÁRIO OU SENHA INVÁLIDOS ❌');
+        }
+    });
+}
+//FUNÇÃO PRINCIPAL TESTANDO COM ASYNC/AWAIT ;-D
+async function executarCT() {
+    console.log('⏳ INICIANDO CENÁRIO DE TESTE ;D');
+    try{
+        console.log('passo 1: Abrindo tela de login...')
+        await aguardar(2000)
+        console.log('Passo 2: Inserindo credênciais...')
+        await aguardar(3000)
+        
+        const token = await simularLogin('admin','123456')
+    }
+}
