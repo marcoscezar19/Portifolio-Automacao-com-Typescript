abbd07ecea0a17fe1c38d2cf6a7057156cc403aa
Marcos Cézar <marcoscezar19@gmail.com>
2026-09-16 15:37:06 +0000

    finalizado arquivo de testes unitários

---
+++ src/modulo2unidade10/atividade.test.ts
@@ -0,0 +1,22 @@
+//ARQUIVO DE TESTES UNITÁRIOS
+import { describe, test, expect } from "vitest";
+import { buscarProdutos, aplicarDescontoSimulado } from "./atividadeImplementarExemplosPraticos";q
+
+describe("Testes Unitários da Atividade", () => {
+
+    test("Deve buscar a lista de produtos da API", async () => {
+        const produtos = await buscarProdutos();
+
+        expect(Array.isArray(produtos)).toBe(true);
+        expect(produtos.length).toBeGreaterThan(0);
+        expect(produtos[0]).toHaveProperty("price");
+        
+    });
+
+    test("Deve aplicar 10% de desconto na promise simulada", async () => {
+        const precoOriginal = 100; 
+        const precoComDesconto = await aplicarDescontoSimulado(precoOriginal);
+
+        expect(precoComDesconto).toBe(90);
+    });
+})
