752cf8e77d12f1835b2320dac6569c4b234d4c27
Marcos Cézar <marcoscezar19@gmail.com>
2026-09-17 13:52:00 +0000

    corrigi linha 21 jason para .json

--- src/aula29/apis2.test.ts
+++ src/aula29/apis2.test.ts
@@ -18,7 +18,7 @@ test('Metodo POST para criar um novo post', async () =>{
     //Testar status code
         expect(res.status).toBe(201)
     //Testar se o retorno é um objeto JSON
-        const dados = await res.jason();
+        const dados = await res.json();
         expect(dados).toHaveProperty('id');
         expect(dados.title).toBe('Meu novo post');
         expect(dados.body).toBe('Conteúdo do meu novo post');
