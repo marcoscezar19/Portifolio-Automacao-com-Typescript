a0b09232dac7e456fe12cbbce213dee833c14e7f
Marcos Cézar <marcoscezar19@gmail.com>
2026-09-17 14:17:03 +0000

    adicionado metodos put e patch

--- src/aula29/apis2.test.ts
+++ src/aula29/apis2.test.ts
@@ -23,3 +23,47 @@ test('Metodo POST para criar um novo post', async () =>{
         expect(dados.title).toBe('Meu novo post');
         expect(dados.body).toBe('Conteúdo do meu novo post');
 });
+
+test('Metodo PUT para criar um novo post', async () =>{
+    const res = await fetch(`${BASE_URL}/posts/1`, {
+        method: 'PUT',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            id: 2,
+            userId: 2,
+            title: 'Meu antigo post',
+            body: 'Conteúdo do meu antigo post'
+        })
+    });
+    //Testar status code
+        expect(res.status).toBe(200);
+    //Testar se o retorno é um objeto JSON
+        const dados = await res.json();
+        expect(dados).toHaveProperty('id');
+        expect(dados.title).toBe('Meu antigo post');
+        expect(dados.body).toBe('Conteúdo do meu antigo post');
+});
+
+test('Metodo PATCH para atualizar um dado', async () =>{
+    const res = await fetch(`${BASE_URL}/posts/2`, {
+        method: 'PATCH',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            id: 3,
+            userId: 3,
+            title: 'NOSSO antigo post',
+            body: 'Conteúdo do meu antigo post'
+        })
+    });
+    //Testar status code
+        expect(res.status).toBe(200);
+    //Testar se o retorno é um objeto JSON
+        const dados = await res.json();
+        expect(dados).toHaveProperty('id');
+        expect(dados.title).toBe('NOSSO antigo post');
+        expect(dados.body).toBe('Conteúdo do meu antigo post');
+});
