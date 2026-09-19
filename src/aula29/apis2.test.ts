e2fbc36e10c5e18022375ec259a95d016e98a8e9
Marcos Cézar <marcoscezar19@gmail.com>
2026-09-19 03:15:01 +0000

    atualizado arquivo src/aula29/apis2.test.ts

--- src/aula29/apis2.test.ts
+++ src/aula29/apis2.test.ts
@@ -15,15 +15,16 @@ test('Metodo POST para criar um novo post', async () =>{
             body: 'Conteúdo do meu novo post'
         })
     });
-    //Testar status code
+    //Testar status code de criação
         expect(res.status).toBe(201)
+
     //Testar se o retorno é um objeto JSON
         const dados = await res.json();
         expect(dados).toHaveProperty('id');
         expect(dados.title).toBe('Meu novo post');
         expect(dados.body).toBe('Conteúdo do meu novo post');
 });
-
+//TESTAR MÉTODO PUT
 test('Metodo PUT para atualizar um novo post', async () =>{
     const res = await fetch(`${BASE_URL}/posts/1`, {
         method: 'PUT',
@@ -31,21 +32,22 @@ test('Metodo PUT para atualizar um novo post', async () =>{
             'Content-Type':'application/json'
         },
         body: JSON.stringify({
-            id: 2,
-            userId: 2,
+            id: 1,
+            userId: 1,
             title: 'MEU TÍTULO ATUALIZADO',
-            body: 'Conteúdo do meu antigo post'
+            body: 'Conteúdo do meu post atualizado'
         })
     });
     //Testar status code
         expect(res.status).toBe(200);
+
     //Testar se o retorno é um objeto JSON
         const dados = await res.json();
         expect(dados).toHaveProperty('id');
         expect(dados.title).toBe('MEU TÍTULO ATUALIZADO');
-        expect(dados.body).toBe('Conteúdo do meu antigo post');
+        expect(dados.body).toBe('Conteúdo do meu post atualizado');
 });
-
+//TESTAR MÉTODO PATCH
 test('Metodo PATCH para atualizar um dado', async () =>{
     const res = await fetch(`${BASE_URL}/posts/1`, {
         method: 'PATCH',
@@ -53,20 +55,17 @@ test('Metodo PATCH para atualizar um dado', async () =>{
             'Content-Type':'application/json'
         },
         body: JSON.stringify({
-            id: 3,
-            userId: 3,
-            title: 'NOSSO antigo post',
-            body: 'Conteúdo do meu antigo post'
+            title: 'Título alterado via patch'
         })
     });
     //Testar status code
         expect(res.status).toBe(200);
+
     //Testar se o retorno é um objeto JSON
         const dados = await res.json();
         expect(dados).toHaveProperty('id');
-        expect(dados.title).toBe('NOSSO antigo post');
-       // expect(dados.body).toBe('Conteúdo do meu antigo post');
-        // expect
+        expect(dados.title).toBe('Título alterado via patch');
+
 });
 
 //Testar metodo delete
